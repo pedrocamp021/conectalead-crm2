19634191a6ed6a37d5c9e5edde3fe7f1c14b6e9e
pedrocamp021 <pedrocamp021@gmail.com>
2025-05-10 10:21:48 -0300

    Corrigindo persistência de sessão com localStorage

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,9 +1,14 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './database.types';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL!;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY!;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Missing Supabase environment variables');
+}
+
+// 👇👇 Aqui forçamos o uso do localStorage na config
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
   auth: {
     persistSession: true,
