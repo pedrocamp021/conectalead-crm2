e355037d7d47eb6e207c6139ae8446e626784ec6
pedrocamp021 <132719870+pedrocamp021@users.noreply.github.com>
2025-05-10 23:42:35 -0300

    Updated dialog.tsx

---
+++ src/components/ui/dialog.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import { Dialog as BaseDialog, DialogTrigger, DialogContent as BaseContent, DialogHeader as BaseHeader, DialogTitle as BaseTitle } from "@radix-ui/react-dialog";
+
+export const Dialog = BaseDialog;
+export const DialogContent = BaseContent;
+export const DialogHeader = BaseHeader;
+export const DialogTitle = BaseTitle;
+export const DialogTriggerButton = DialogTrigger;
